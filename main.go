7657a28d1cd2cd2aecc26cdb052d99dbb3859fcc
Alexander Sheiko <alexander.sheiko@mgid.com>
2022-06-16 11:40:07 +0300

    Refactoring Small changes and fixes

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"net/url"
 	"os"
 	"path"
+	"strings"
 	"sync"
 	"time"
 
@@ -16,18 +17,21 @@ import (
 )
 
 var args struct {
-	Source      string `arg:"positional,required" help:"Source bucket"`
-	Destination string `arg:"positional,required" help:"Destination bucket"`
-	Concurrency int    `arg:"-c,--concurrency" placeholder:"NUM" help:"Number of concurrent transfers" default:"10"`
-	Recursive   bool   `arg:"-r,--recursive" help:"Recursively copy all objects in the source bucket"`
-	Region      string `arg:"--region" help:"AWS region" default:"us-east-1"`
-	Timeout     int    `arg:"-t,--timeout" placeholder:"SECONDS" help:"Copy timeout in seconds" default:"60"`
-	Wait        bool   `arg:"-w,--wait" help:"Wait for the item to be copied"`
+	Source       string `arg:"positional,required" help:"Source bucket"`
+	Destination  string `arg:"positional,required" help:"Destination bucket"`
+	ACL          string `arg:"-a,--acl" help:"ACL to apply to the copied object"`
+	Concurrency  int    `arg:"-c,--concurrency" placeholder:"NUM" help:"Number of concurrent transfers" default:"10"`
+	Recursive    bool   `arg:"-r,--recursive" help:"Recursively copy all objects in the source bucket"`
+	Region       string `arg:"--region" help:"AWS region" default:"us-east-1"`
+	StorageClass string `arg:"--storage-class" placeholder:"CLASS" help:"Storage class to apply to the copied object" default:"STANDARD"`
+	Timeout      int    `arg:"-t,--timeout" placeholder:"SECONDS" help:"Copy timeout in seconds" default:"60"`
+	Wait         bool   `arg:"-w,--wait" help:"Wait for the item to be copied"`
 }
 
 func main() {
 	arg.MustParse(&args)
 	logerr := log.New(os.Stderr, "", 0)
+	loginfo := log.New(os.Stdout, "", 0)
 
 	source, err := url.Parse(args.Source)
 	if err != nil {
@@ -69,14 +73,53 @@ func main() {
 		defer cancelFn()
 	}
 
-	// List all objects in the source bucket
-	objects := []string{}
+	semaphore := make(chan struct{}, args.Concurrency)
+	var wg sync.WaitGroup
+
+	// Object copy function.
+	copyObject := func(sourceBucket, sourcePath, targetBucket, targetPath string) {
+		if args.Recursive {
+			defer func() { <-semaphore }()
+			defer wg.Done()
+		}
+		// Copy the item from the source bucket to the destination bucket.
+		_, err := svc.CopyObjectWithContext(ctx, &s3.CopyObjectInput{
+			CopySource:   aws.String(url.QueryEscape(path.Join(sourceBucket, sourcePath))),
+			Bucket:       aws.String(targetBucket),
+			Key:          aws.String(targetPath),
+			ACL:          aws.String(args.ACL),
+			StorageClass: aws.String(args.StorageClass),
+		})
+		if err != nil {
+			logerr.Printf("Failed to copy object %s: %v\n", sourcePath, err)
+			return
+		}
+		// Wait for the item to be copied
+		if args.Wait {
+			err = svc.WaitUntilObjectExistsWithContext(ctx, &s3.HeadObjectInput{
+				Bucket: aws.String(targetBucket),
+				Key:    aws.String(targetPath),
+			})
+			if err != nil {
+				logerr.Printf("Failed to wait for object %s: %v\n", targetPath, err)
+				return
+			}
+		}
+		loginfo.Printf("Item %q successfully copied from bucket %q to bucket %q\n", sourcePath, sourceBucket, targetBucket)
+	}
+
 	if args.Recursive {
+		// List all objects in the source bucket and copy them to the target bucket
 		err = svc.ListObjectsPagesWithContext(ctx, &s3.ListObjectsInput{
 			Bucket: aws.String(source.Host),
+			Prefix: aws.String(strings.TrimPrefix(source.Path, "/")),
 		}, func(p *s3.ListObjectsOutput, lastPage bool) bool {
 			for _, o := range p.Contents {
-				objects = append(objects, aws.StringValue(o.Key))
+				sourcePath := aws.StringValue(o.Key)
+				targetPath := path.Join(target.Path, sourcePath)
+				wg.Add(1)
+				semaphore <- struct{}{}
+				go copyObject(source.Host, sourcePath, target.Host, targetPath)
 			}
 			return true // continue paging
 		})
@@ -84,44 +127,10 @@ func main() {
 			logerr.Printf("Failed to list objects for source bucket %s: %v\n", source.Host, err)
 			os.Exit(5)
 		}
+		wg.Wait()
 	} else {
-		objects = []string{source.Path}
+		// Copy onces the item to the target bucket
+		targetPath := path.Join(target.Path, source.Path)
+		copyObject(source.Host, source.Path, target.Host, targetPath)
 	}
-
-	semaphore := make(chan struct{}, args.Concurrency)
-	var wg sync.WaitGroup
-
-	// Copy the item from the source bucket to the destination bucket.
-	for _, sourcePath := range objects {
-		targetPath := path.Join(target.Path, sourcePath)
-		semaphore <- struct{}{}
-		wg.Add(1)
-		go func(sourceBucket, sourcePath, targetBucket, targetPath string) {
-			defer func() { <-semaphore }()
-			defer wg.Done()
-			// Copy the item from the source bucket to the destination bucket.
-			_, err := svc.CopyObjectWithContext(ctx, &s3.CopyObjectInput{
-				CopySource: aws.String(sourceBucket + "/" + sourcePath),
-				Bucket:     aws.String(targetBucket),
-				Key:        aws.String(targetPath),
-			})
-			if err != nil {
-				logerr.Printf("Failed to copy object %s: %v\n", sourcePath, err)
-				return
-			}
-			// Wait for the item to be copied
-			if args.Wait {
-				err = svc.WaitUntilObjectExistsWithContext(ctx, &s3.HeadObjectInput{
-					Bucket: aws.String(targetBucket),
-					Key:    aws.String(targetPath),
-				})
-				if err != nil {
-					logerr.Printf("Failed to wait for object %s: %v\n", targetPath, err)
-					return
-				}
-			}
-			log.Printf("Item %q successfully copied from bucket %q to bucket %q\n", sourcePath, sourceBucket, targetBucket)
-		}(source.Host, sourcePath, target.Host, targetPath)
-	}
-	wg.Wait()
 }
